013dde1e75e41a7c129637e7f8c7aa8e60d49670
Andrew Creekmore <andrew.creekmore@me.com>
2024-02-23 11:42:13 -0600

    added exception handler to desktopUI app startup;

--- DesktopUI/TaskFocusDesktop/App.xaml.cs
+++ DesktopUI/TaskFocusDesktop/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TaskFocusDesktop
 {
@@ -13,5 +14,38 @@ namespace TaskFocusDesktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // define application exception handler
+            Application.Current.DispatcherUnhandledException +=
+                AppDispatcherUnhandledException;
+
+            // defer other startup processing to base class
+            base.OnStartup(e);
+        }
+
+
+        private void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            runException(e.Exception);
+
+            e.Handled = true;
+        }
+
+        void runException(Exception ex)
+        {
+            MessageBox.Show(
+                String.Format(
+                    "{0} Error:  {1}\r\n\r\n{2}",
+                    ex.Source, ex.Message, ex.StackTrace,
+                    "Initialize Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error));
+
+            if (ex.InnerException != null)
+            {
+                runException(ex.InnerException);
+            }
+        }
     }
 }
