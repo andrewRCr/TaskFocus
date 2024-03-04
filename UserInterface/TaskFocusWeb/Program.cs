5202c1567956d5260f603e0c9da27fafb3c57fab
Andrew Creekmore <andrew.creekmore@me.com>
2024-03-04 15:54:06 -0600

    added favicons; added MudBlazor; added expanding navgroups for Projects/Contexts to NavMenu;

--- UserInterface/TaskFocusWeb/Program.cs
+++ UserInterface/TaskFocusWeb/Program.cs
@@ -2,6 +2,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using TaskFocusUI.Library.API;
 using TaskFocusUI.Library.Models;
 using TaskFocusWeb.Authentication;
@@ -30,6 +31,7 @@ namespace TaskFocusWeb
             builder.Services.AddTransient<IContextEndpoint, ContextEndpoint>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
