553d14901673373ea73389be12c8ee33c191fe66
Andrew Creekmore <andrew.creekmore@me.com>
2025-02-24 11:10:22 -0600

    desktopUI: due date picker foreground color now depends on overdue status, similar to webUI;

---
+++ UserInterface/TaskFocusDesktop/Utilities/IsTaskOverdueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TaskFocusDesktop.Utilities
+{
+    class IsTaskOverdueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime? dueDateValue = (DateTime)value;
+            if (dueDateValue != null) { return dueDateValue < DateTime.Now; }
+
+            throw new InvalidOperationException("The target must be a DateTime!");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
